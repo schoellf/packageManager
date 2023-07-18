7af7b79cb652dcd1692f98564de82e0065c6f1e9
Benjamin Terbul <terbulb@edu.htl-villach.at>
2023-07-18 08:13:22 +0200

    chngeTerbul

--- frontend-react/src/components/PackageList.js
+++ frontend-react/src/components/PackageList.js
@@ -1,9 +1,9 @@
 import "./PackageList.css"
 
-export default function PackageList({packages, selectedPaths, setSelectedPaths}){
+export default function PackageList({packages, selectedPaths, setSelectedPaths, onLoadMore}){
  
   function handleCheckedOrUncheckedPackage(e, pkg) {
-
+    console.log("handle")
     if(e.target.checked) {
       //add to post request
       setSelectedPaths([...selectedPaths, pkg.attributes.versions[Object.keys(pkg.attributes.versions)[0]]])
@@ -17,13 +17,14 @@ export default function PackageList({packages, selectedPaths, setSelectedPaths})
   }
 
 
+
   return (
-    <div style={{ display: 'flex', flexWrap: 'wrap', height: "fit-content" }}>
+    <div className="listPkgs" style={{ display: 'flex', flexWrap: 'wrap', height: "fit-content", maxHeight: "100%", overflow:"auto"}}>
     {packages?.map((pkg) => (
       <div
         key={pkg.id}
         className="package-card"
-        style={{ flex: '0 0 170px', margin: '20px' }}
+        style={{ flex: '0 0 150px', margin: '20px' }}
       >
         <div
           className="packageDescr"
@@ -35,9 +36,11 @@ export default function PackageList({packages, selectedPaths, setSelectedPaths})
           <h2>{pkg.attributes.name}</h2>
         </div>
         <small>{pkg.attributes.url}</small>
-        <input type="checkbox" style={{ float: 'right' }} onChange={e => {handleCheckedOrUncheckedPackage(e, pkg)}}/>
+        <input type="checkbox" style={{ float: 'right' }} onChange={e => {handleCheckedOrUncheckedPackage(e, pkg)}} checked={selectedPaths.indexOf(pkg.attributes.versions[Object.keys(pkg.attributes.versions)[0]])!=-1}/>
       </div>
     ))}
+    
+      <button style={{margin: "20px", width: "150px", height: "80px"}} onClick={()=>{if(onLoadMore){onLoadMore()}}}>Load More</button>
     </div>
   );
 }
--- frontend-react/src/pages/PackagePage.js
+++ frontend-react/src/pages/PackagePage.js
@@ -6,9 +6,16 @@ import "./PackagePage.css"
 import "../../node_modules/react-bootstrap/Button"
 import Button from '../../node_modules/react-bootstrap/Button';
 
+
+export default function PackagePage() {
+const DEFAULTDATASIZE = 20;
+const [dataSize, setDataSize] = useState(DEFAULTDATASIZE);
+const [searchText, setSearchText] = useState("");
+const [queryFilter, setQueryFilter] = useState("");
+
 const PACKAGES = gql`
 query GetPackages {
-  packages {
+  packages ( filters:{name:{containsi:"${queryFilter}"}}, pagination:{page:1, pageSize: ${dataSize}}) {
     data {
       id,
       attributes { 
@@ -30,24 +37,44 @@ query GetPackages {
   }
 }`
 
-
-export default function PackagePage() {
   const { loading, error, data } = useQuery(PACKAGES);
 
   const [selectedPage, setSelectedPage] = useState("AV");
 
-  const [searchText, setSearchText] = useState("");
-  const searchedPackagesAV = data?.packages.data.filter(pkg => pkg.attributes.name.toUpperCase().includes(searchText.toUpperCase()) || searchText === "")
   const searchedPackagesAP = data?.approvedPackages.data.filter(pkg => pkg.attributes.name.toUpperCase().includes(searchText.toUpperCase()) || searchText === "")
 
-  // selectedPage=="AP"?[]:
+  const [selectedPaths, setSelectedPaths] = useState([])
+
+
   if (loading) return <p>Loading...</p>
   if (error) return <p>error!</p>
 
+  function handleLoadMore(){
+    setDataSize(dataSize+DEFAULTDATASIZE);
+  }
 
+  function setQueryText(){
+    setDataSize(DEFAULTDATASIZE);
+    setQueryFilter(searchText);
+  }
+
+  function handleEnter(e){
+    if(e.code == "Enter"){
+      setQueryText();
+    }
+  }
+
+  function handleBtnSubmit(e){
+
+    if(selectedPage == "AV"){
+      console.log("push")
+    }else{
+      console.log("delete")
+    }
+  }
 
   return (
-    <div style={{height:"90%", overflow:"hidden"}}>
+    <div style={{height:"90%", overflow:"hidden"}} onClick={()=>console.log(selectedPaths)}>
       <div className='searchbar'> 
         <input
           style={{
@@ -55,24 +82,26 @@ export default function PackagePage() {
             height: '30px',
             borderRadius: '5px',
             border: 'solid darkblue',
-            marginBottom: '10px'
+            gridRow: 1,
+            justifySelf: selectedPage=="AV"?"right":"center"
           }}
           type="text"
           placeholder="Suchen..."
           value={searchText}
           onChange={(e) => setSearchText(e.target.value)}
+          onKeyDown={handleEnter}
         />
-      </div>
+        {selectedPage == "AV" && <button style={{gridRow:1, justifySelf: "left", height: "30px", marginLeft: "10px"}} onClick={setQueryText}><i className="bi bi-arrow-return-left"></i></button>}      </div>
       <div id='divChangeList'>
-        <Button className='btnChangeList' style={{justifySelf: "right", backgroundColor: selectedPage=="AP"?"#b49bc5":"transparent"}} onClick={()=>setSelectedPage("AP")}>Approved</Button>
-        <Button className='btnChangeList' style={{justifySelf: "left", backgroundColor: selectedPage=="AV"?"#b49bc5":"transparent"}} onClick={()=>setSelectedPage("AV")}>Available</Button>
+        <Button className='btnChangeList' style={{justifySelf: "right", backgroundColor: selectedPage=="AP"?"#b49bc5":"transparent"}} onClick={()=>{setSelectedPage("AP");setSelectedPaths([])}}>Approved</Button>
+        <Button className='btnChangeList' style={{justifySelf: "left", backgroundColor: selectedPage=="AV"?"#b49bc5":"transparent"}} onClick={()=>{setSelectedPage("AV");setSelectedPaths([])}}>Available</Button>
       </div>
       <div id='pageSlider' style={{position: "relative",left: selectedPage=="AP"?"0%":"-100%"}}>
         <PackageList packages={searchedPackagesAP} selectedPaths={[]} setSelectedPaths={()=>{}}></PackageList>
-        <PackageList packages={searchedPackagesAV} selectedPaths={[]} setSelectedPaths={()=>{}}></PackageList>
+        <PackageList packages={data?.packages?.data} selectedPaths={selectedPaths} setSelectedPaths={setSelectedPaths} onLoadMore={handleLoadMore}></PackageList>
       </div>
       <div id='divSubmit'>
-          <button id='btnSubmit'>Remove Selected</button>
+          <button id='btnSubmit' onClick={handleBtnSubmit}>{selectedPage=="AV"?"Add Selected":"Remove Selected"}</button>
       </div>
     </div>
   );
